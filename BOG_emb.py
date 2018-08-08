0ebca5c8ba22f2beae650046028d3b11dfda2c53
solacex <lgr2819@outlook.com>
2018-08-08 13:25:45 +1000

    modified:   BOG_emb.py 	deleted:    bog_dic.pkl 	deleted:    msrvtt_elmo_captions.pkl 	deleted:    msrvtt_elmo_captions_MUl.pkl 	w2v.py

--- BOG_emb.py
+++ BOG_emb.py
@@ -1,18 +1,18 @@
+from sklearn.externals import joblib
+import numpy as np
 import pickle
 with open('./bog_dic.pkl','rb') as m:
     bog = pickle.load(m)
-with open('./data/msrvtt_captions.pkl','rb') as f:
+with open('../data/msrvtt_captions.pkl','rb') as f:
     w = pickle.load(f)
 
 
-index_bog={}
-cnt = 0
-for k in bog.keys():
-    index_bog[k]=cnt
-    cnt+=1
+keys=list(bog.keys())
+keys=np.array(keys)
+length_dic=len(keys)
 count=0
 repre={}
-length_dic=len(bog)
+
 
 for k in w.keys():
     sen_list=w[k]
@@ -22,13 +22,11 @@ for k in w.keys():
     i = 0
     for s in sen_list:
         tmp_word = s.split()
-            for word in tmp_word:
-                tmp[i,index_bog[word]]+=1
+        for word in tmp_word:
+            tmp[i,np.argwhere(keys==word)]+=1
     repre[k] = tmp
-    if count%5==0:
+    if count%500==0:
         print(count)
 
 print(len(repre))
-with open('./bog_msrvtt_caption.pkl','wb') as m:
-    pickle.dump(repre,m)
-m.close()
+joblib.dump(repre,'bog_msrvtt_caption.pkl')
